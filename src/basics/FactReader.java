7480f2cd087146b5ce36c3d18d2936cd8406f147
smetzger <smetzger@mpi-inf.mpg.de>
2012-03-01 11:40:47 +0000

    bit additional documentation

--- src/basics/FactReader.java
+++ src/basics/FactReader.java
@@ -2,7 +2,16 @@ package basics;
 
 import java.io.IOException;
 
-
+/**
+ * FactReader interface - YAGO2S
+ * 
+ * Provides an interface for fact deserialisation. 
+ * Note that you will need to choose the interface implementation 
+ * that fits to the FactWriter implementation used to generate the serialised fact representation.
+ * 
+ * 
+ * 
+ */
 public interface FactReader {
   public Fact read() throws IOException;
   public void close()throws IOException;
--- src/basics/FactWriter.java
+++ src/basics/FactWriter.java
@@ -2,7 +2,16 @@ package basics;
 
 import java.io.IOException;
 
-
+/**
+ * FactWriter interface - YAGO2S
+ * 
+ * Provides an interface for fact serialisation, 
+ * allowing you to materialize a set of YAGO facts 
+ * e.g. as N3/N4 file depending on the chosen implementation. 
+ * 
+ * 
+ * 
+ */
 public interface FactWriter {
   public void write(Fact f) throws IOException;
   public void close()throws IOException;
