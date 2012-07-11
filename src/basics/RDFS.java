c885a3216ba41e82dc3e9613b0c9d1dac6a7ba34
suchanek <suchanek@mpi-inf.mpg.de>
2012-07-11 16:27:44 +0000

    added constants, changed rdfs:domain of hassynset to class

--- src/basics/RDFS.java
+++ src/basics/RDFS.java
@@ -19,4 +19,7 @@ public class RDFS {
 	public static final String subpropertyOf = "rdfs:subPropertyOf";
   public static final String property = "rdf:Property";
   public static final String statement="rdf:Statement";
+  public static final String object = "rdf:object";
+  public static final String predicate = "rdf:predicate";
+  public static final String subject = "rdf:subject";
 }
