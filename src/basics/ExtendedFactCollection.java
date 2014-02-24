6df98134df0083666775b0b5c7683c49a3370bfb
fmahdiso <fmahdiso@mpi-inf.mpg.de>
2014-02-24 21:51:27 +0000

    Reformat

--- src/basics/ExtendedFactCollection.java
+++ src/basics/ExtendedFactCollection.java
@@ -1,29 +1,15 @@
 package basics;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.AbstractSet;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
-
-import javatools.administrative.Announce;
-import javatools.administrative.D;
-import javatools.filehandlers.FileSet;
 
 /**
  * Class ExtendedFactCollection - YAGO2S
  * 
- * Represents a collection of facts, indexes them with extended functionality
+ * Represents a collection of facts, indexes them with extended functionality.
  * 
  * @author Farzaneh Mahdisoltani
  */
@@ -34,31 +20,26 @@ public class ExtendedFactCollection extends FactCollection {
 
   /** Adds a fact, does not check for duplicates*/
   protected synchronized boolean justAdd(final Fact fact) {
-	  if(!super.justAdd(fact))
-		  return false;
-	  
-	  if (!obindex.containsKey(fact.arg2)) obindex.put(fact.arg2, Collections.synchronizedList(new ArrayList<Fact>(1)));
-	  obindex.get(fact.arg2).add(fact);
+    if (!super.justAdd(fact)) return false;
 
-	  return (true);
+    if (!obindex.containsKey(fact.arg2)) obindex.put(fact.arg2, Collections.synchronizedList(new ArrayList<Fact>(1)));
+    obindex.get(fact.arg2).add(fact);
+
+    return (true);
   }
-
 
   /** Returns facts with matching second arg */
   public List<Fact> getFactsWithObject(String arg2) {
-	  List<Fact> result = new ArrayList<Fact>();
-	  if (obindex.containsKey(arg2)) 
-		  result=obindex.get(arg2);
-	  return (result);
+    List<Fact> result = new ArrayList<Fact>();
+    if (obindex.containsKey(arg2)) result = obindex.get(arg2);
+    return (result);
   }
-
 
   /** Removes a fact */
   public synchronized boolean remove(Object f) {
-	  if(!super.remove(f)) 
-		  return false;
-	  obindex.get(((Fact)f).arg2).remove(((Fact)f));
-	  return true;
+    if (!super.remove(f)) return false;
+    obindex.get(((Fact) f).arg2).remove(((Fact) f));
+    return true;
   }
 
   /** Removes all facts */
@@ -67,9 +48,13 @@ public class ExtendedFactCollection extends FactCollection {
     obindex.clear();
   }
 
-
-
- 
-
+  /** Loads from a FactSource */
+  public static ExtendedFactCollection loadFacts(FactSource factSource) {
+    ExtendedFactCollection result = new ExtendedFactCollection();
+    for (Fact f : factSource) {
+      result.add(f);
+    }
+    return result;
+  }
 
 }
