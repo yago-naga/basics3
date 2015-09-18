4a4e654b30acbb38724c58b1b3b5419043c3eaef
suchanek <suchanek@mpi-inf.mpg.de>
2015-09-18 13:19:04 +0000

    caching dictionaries

--- src/basics/YAGO.java
+++ src/basics/YAGO.java
@@ -15,27 +15,51 @@ package basics;
 
 public class YAGO {
 
-	public static final String function = "owl:FunctionalProperty";
-	public static final String functionInTime = "<yagoFunctionInTime>";
-	public static final String url = "<yagoURL>";
-	public static final String string = "xsd:string";
-	public static final String yago = "<YAGO_(database)>";
-	public static final String hasNumber = "<hasNumber>";
-	public static final String entity = "owl:Thing";
-	public static final String person = "<wordnet_person_100007846>";
-	public static final String organization = "<wordnet_organization_108008335>";
-	public static final String location = "<yagoGeoEntity>";
-	public static final String artifact = "<wordnet_artifact_100021939>";
-	public static final String physicalEntity = "<wordnet_physical_entity_100001930>";
-	public static final String abstraction = "<wordnet_abstraction_100002137>";
-	public static final String extractionSource = "<extractionSource>";
-	public static final String extractionTechnique = "<extractionTechnique>";
-	public static final String languageString = "<yagoLanString>";
-	public static final String hasConfidence = "<hasConfidence>";
-	public static final String building = "<wordnet_building_102913152>";
-	public static final String hasGloss = "<hasGloss>";
-	public static final String hasPreferredName = "skos:prefLabel";
-	public static final String isPreferredMeaningOf = "<isPreferredMeaningOf>";
-	public static final String integer = "xsd:integer";
-	public static final String decimal = "xsd:decimal";
+  public static final String function = "owl:FunctionalProperty";
+
+  public static final String functionInTime = "<yagoFunctionInTime>";
+
+  public static final String url = "<yagoURL>";
+
+  public static final String string = "xsd:string";
+
+  public static final String yago = "<YAGO_(database)>";
+
+  public static final String hasNumber = "<hasNumber>";
+
+  public static final String entity = "owl:Thing";
+
+  public static final String person = "<wordnet_person_100007846>";
+
+  public static final String organization = "<wordnet_organization_108008335>";
+
+  public static final String location = "<yagoGeoEntity>";
+
+  public static final String artifact = "<wordnet_artifact_100021939>";
+
+  public static final String physicalEntity = "<wordnet_physical_entity_100001930>";
+
+  public static final String abstraction = "<wordnet_abstraction_100002137>";
+
+  public static final String extractionSource = "<extractionSource>";
+
+  public static final String extractionTechnique = "<extractionTechnique>";
+
+  public static final String languageString = "<yagoLanString>";
+
+  public static final String hasConfidence = "<hasConfidence>";
+
+  public static final String building = "<wordnet_building_102913152>";
+
+  public static final String hasGloss = "<hasGloss>";
+
+  public static final String hasPreferredName = "skos:prefLabel";
+
+  public static final String isPreferredMeaningOf = "<isPreferredMeaningOf>";
+
+  public static final String integer = "xsd:integer";
+
+  public static final String decimal = "xsd:decimal";
+
+  public static final String hasTranslation = "<_hasTranslation>";
 }
