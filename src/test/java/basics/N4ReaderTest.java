25415885675da589f65f6ad674a807066e29e4c3
Adrian Rivero <arivero.smaugh@gmail.com>
2019-01-08 22:34:13 +0100

    Fixed typo in variable name. Added to .gitignore generated files of IntelliJ IDEA.

--- src/test/java/basics/N4ReaderTest.java
+++ src/test/java/basics/N4ReaderTest.java
@@ -28,9 +28,9 @@ public class N4ReaderTest {
   public void test00() throws IOException {
     String testFileNumber = "00";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
 
   
@@ -38,27 +38,27 @@ public class N4ReaderTest {
   public void test01() throws IOException {
     String testFileNumber = "01";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test02() throws IOException {
     String testFileNumber = "02";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test03() throws IOException {
     String testFileNumber = "03";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -66,9 +66,9 @@ public class N4ReaderTest {
   public void test04() throws IOException {
     String testFileNumber = "04";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -76,18 +76,18 @@ public class N4ReaderTest {
   public void test05() throws IOException {
     String testFileNumber = "05";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test06() throws IOException {
     String testFileNumber = "06";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -95,18 +95,18 @@ public class N4ReaderTest {
   public void test07() throws IOException {
     String testFileNumber = "07";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test08() throws IOException {
     String testFileNumber = "08";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -114,36 +114,36 @@ public class N4ReaderTest {
   public void test09() throws IOException {
     String testFileNumber = "09";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test10() throws IOException {
     String testFileNumber = "10";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test11() throws IOException {
     String testFileNumber = "11";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test12() throws IOException {
     String testFileNumber = "12";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -151,72 +151,72 @@ public class N4ReaderTest {
   public void test13() throws IOException {
     String testFileNumber = "13";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test14() throws IOException {
     String testFileNumber = "14";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test15() throws IOException {
     String testFileNumber = "15";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test16() throws IOException {
     String testFileNumber = "16";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test17() throws IOException {
     String testFileNumber = "17";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test18() throws IOException {
     String testFileNumber = "18";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test19() throws IOException {
     String testFileNumber = "19";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test20() throws IOException {
     String testFileNumber = "20";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -224,9 +224,9 @@ public class N4ReaderTest {
   public void test21() throws IOException {
     String testFileNumber = "21";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -234,9 +234,9 @@ public class N4ReaderTest {
   public void test22() throws IOException {
     String testFileNumber = "22";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -244,45 +244,45 @@ public class N4ReaderTest {
   public void test23() throws IOException {
     String testFileNumber = "23";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test24() throws IOException {
     String testFileNumber = "24";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test25() throws IOException {
     String testFileNumber = "25";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test26() throws IOException {
     String testFileNumber = "26";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test27() throws IOException {
     String testFileNumber = "27";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -290,18 +290,18 @@ public class N4ReaderTest {
   public void test28() throws IOException {
     String testFileNumber = "28";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
   public void test29() throws IOException {
     String testFileNumber = "29";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -309,9 +309,9 @@ public class N4ReaderTest {
   public void test30() throws IOException {
     String testFileNumber = "30";
     String testFilePath = RESOURCESPATH + "/input/test-" + testFileNumber + ".ttl";
-    String exectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
+    String expectedOutputFilePath = RESOURCESPATH + "/output/test-" + testFileNumber + ".out";
     
-    runAndCompare(testFilePath, exectedOutputFilePath);
+    runAndCompare(testFilePath, expectedOutputFilePath);
   }
   
   @Test
@@ -326,7 +326,7 @@ public class N4ReaderTest {
     // I have nothing to compare it to. But It reads all facts with no error.
   }
 
-  private void runAndCompare(String testFilePath, String exectedOutputFilePath) throws IOException {
+  private void runAndCompare(String testFilePath, String expectedOutputFilePath) throws IOException {
     List<Fact> expected = new ArrayList<>();
     List<Fact> actual = new ArrayList<>();
     
@@ -335,7 +335,7 @@ public class N4ReaderTest {
       actual.add(nr.next());
     nr.close();
     
-    nr = new N4Reader(new File(exectedOutputFilePath));
+    nr = new N4Reader(new File(expectedOutputFilePath));
     while(nr.hasNext())
       expected.add(nr.next());
     nr.close();
